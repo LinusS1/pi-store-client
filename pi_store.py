a00d5c1a2b327047e0e85b5153fe391880b926fe
Linus <LinusS1@users.noreply.github.com>
2017-11-28 18:47:28 -0800

    style client

--- pi_store.py
+++ pi_store.py
@@ -22,14 +22,11 @@ class Store(Gtk.Window):
 		#~ self.webview.open("https://piStore.pythonanywhere.com")# Production
 		self.webview.connect('download-requested', self.download_requested)
 		self.webview.connect('mime-type-policy-decision-requested', self.policy_decision_requested)
-		
 		#Progress bar
 		self.progressbar = Gtk.ProgressBar()
 		vbox.pack_start(self.progressbar, True, True, 0)
-		
 		self.timeout_id = GObject.timeout_add(50, self.on_timeout, None)
 		self.activity_mode = False
-		
 		# Random config
 		self.progressbar.set_pulse_step(0.03)
 		self.set_default_size(800, 600)
@@ -48,10 +45,8 @@ class Store(Gtk.Window):
 	def on_timeout(self, user_data):
 		if self.activity_mode:
 			self.progressbar.pulse()
-			
 		self.progressbar.set_text("Installing...")
 		self.progressbar.set_show_text(self.activity_mode)
-
 		return True
 
 win = Store()
